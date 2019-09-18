a2c5cedc9480245e95a7d2300b49993bf2248bb3
Koharu-Sato <54939418+Koharu-Sato@users.noreply.github.com>
2019-09-18 17:48:17 +0900

    user add and user remove completed!

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,10 +2,12 @@ $(function() {
 
   let search_user_list = $('#user-search-result')
 
+  let chat_group_user_list = $('.chat-group-users.js-add-user')
+
   function appendUser(user) {
     let html = `<div class="chat-group-user clearfix">
-                 <p class="chat-group-user__name">${ user.name } </p>
-                 <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                  <p class="chat-group-user__name">${ user.name } </p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                 </div>`
     search_user_list.append(html);
   }
@@ -17,8 +19,18 @@ $(function() {
     search_user_list.append(html)
   }
 
+  function appendChatGroup(hash) {
+    let html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${ hash.id }'>
+                    <p class='chat-group-user__name'>${ hash.name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    chat_group_user_list.append(html)
+  }
+
   $('#user-search-field').on("keyup", function() {
     let input = $('#user-search-field').val();
+
     
     $.ajax({
       type: 'GET',
@@ -41,5 +53,16 @@ $(function() {
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     });
+
+    $(document).on("click", ".user-search-add.chat-group-user__btn.chat-group-user__btn--add", function() {
+       let id = $(this).attr('data-user-id');
+       let name = $(this).attr('data-user-name');
+       let hash = { id: id, name: name }
+      $(this).closest('div').remove();
+      appendChatGroup(hash)
+    });
+  });
+  $(document).on("click", ".user-search-remove.chat-group-user__btn.chat-group-user__btn--remove.js-remove-btn", function() {
+    $(this).closest('div').remove();
   });
 });
